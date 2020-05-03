79f2a78f8c8a85071ccf7c0dca26aed5f220bce1
fp_testuser <fp.vjti@gmail.com>
2020-05-03 15:11:35 +0530

    CopyArray to another Array

---
+++ Arrays/src/com/javaHK/Arrays/CopyArray.java
@@ -0,0 +1,17 @@
+package com.javaHK.Arrays;
+
+import java.util.Arrays;
+
+public class CopyArray {
+    public static void main(String[] args) {
+	int[] ia1 = {2,4,6,8};
+	int[] ia2 = new int[ia1.length];
+	
+	System.out.println(Arrays.toString(ia1));
+	
+	for (int i = 0; i < ia1.length; i++) {
+		ia2[i] = ia1[i];
+	}
+	System.out.println(Arrays.toString(ia2));
+    }
+}
